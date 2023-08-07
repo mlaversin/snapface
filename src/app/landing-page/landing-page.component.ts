9f15e379659b7a0ce0a7f2e1057a67cbab02dafa
Morgan Laversin <morgan.laversin@alten.com>
2023-08-07 10:28:40 +0200

    add a template-driven form

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  userEmail!: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onContinue(): void {
     this.router.navigateByUrl('facesnaps');
   }
 
+  onSubmitForm(myform: NgForm) {
+    console.log(myform.value);
+  }
 }
